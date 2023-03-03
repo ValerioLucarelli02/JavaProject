c95e71e16777caf85a762a3cf3d1da84275fa511
ValerioLucarelli02 <126474133+ValerioLucarelli02@users.noreply.github.com>
2023-03-03 12:56:29 +0100

    Add files via upload

---
+++ FileVerifica.java
@@ -0,0 +1,64 @@
+package main;
+
+import java.util.Scanner;
+import java.lang.reflect.Array;
+public class FileVerifica {
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		//PRIMA DOMANDA
+	    //Scanner valore = new Scanner (System.in);
+	
+		
+	    // System.out.print(" inserire primo valore ");  //richiedo il primo valore da inserire all'utente
+	    // int x = valore.nextInt(); //utilizzo questo metodo per far scrivere all'utente il primo numero
+	    //System.out.print(" inserire secondo valore "); //richiedo il secondo valore da inserire all'utente
+	    //int y = valore.nextInt(); //utilizzo questo metodo per far scrivere all'utente il secondo numero
+	    //int somma = x+y; //creo somma 
+	    //System.out.println("il risultato è il seguente "+somma); //stampo il risultato della somma, data dai numeri inseriti dall'utente
+	    
+	    
+	    
+	 
+		
+		//SECONDA DOMANDA
+		//int x=5; //creo il primo valore 5 
+		//int y=7; //creo il secondo valore 7
+		//int somma = x+y; //creo un metodo che mi possa eseguire la somma
+		//System.out.println(somma); //stampo il metodo somma creato in precedenza
+		//per stampare il risultato dell'operazione utilizzata
+      
+		
+		//TERZA DOMANDA E QUARTA DOMANDA UNITE
+
+		
+		  // int valori[]={2,4,6,8,10}; //inserisco i valori di cui farò la media
+		  // int a; //dichiaro l'intero a
+		  // double somma, media; //utilizzo la variabile double per dichiarare somma e media
+
+	      // somma=0; //dichiaro il valore di somma vuoto
+
+	      // for(a=0; a<=valori.length-1; a=a+1) { 
+		  //somma=somma+valori[a];
+		  //}
+
+		 //media=somma/valori.length;
+
+         //System.out.println("la media tra i valori è la seguente = " + media );
+		 //stampo la media dei valori tramite l'array di elementi interi, trasformandolo in una media di double
+	
+		//QUINTA E SESTA DOMANDA
+	
+	    //String frase = "ciao";  //INSERISCO LA FRASE CIAO COME VALORE
+	    //System.out.println(frase); //La stampo cosi com'è
+	    //StringBuilder stringBuilder = new StringBuilder(frase); 
+	    //frase = stringBuilder.reverse().toString(); //inserisco il metodo per invertirla
+	    //System.out.println(frase); //printo la frase invertita
+
+
+}
+
+}
